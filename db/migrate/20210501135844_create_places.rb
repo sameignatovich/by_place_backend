c12b92b8a0260df55da3b0cb5a824747e34e7e32
Nikita Ignatovich <nikita@ignatovich.me>
2021-05-01 18:37:11 +0300

    Schema updates

--- db/migrate/20210501135844_create_places.rb
+++ db/migrate/20210501135844_create_places.rb
@@ -1,10 +1,11 @@
 class CreatePlaces < ActiveRecord::Migration[6.1]
   def change
     create_table :places, id: :uuid do |t|
-      t.string :name
-      t.string :uri
+      t.string :name, null: false
+      t.string :uri, null: false
 
       t.timestamps
     end
+    add_index :places, :uri, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,10 +45,11 @@ ActiveRecord::Schema.define(version: 2021_05_01_135844) do
   end
 
   create_table "places", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name"
-    t.string "uri"
+    t.string "name", null: false
+    t.string "uri", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["uri"], name: "index_places_on_uri", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
