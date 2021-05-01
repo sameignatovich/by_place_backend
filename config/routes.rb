3b26aa847970c2ec4bf26cfe0610c72589d890e9
Nikita Ignatovich <nikita@ignatovich.me>
2021-05-01 18:35:04 +0300

    Moved Places to v1 scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :places
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    resources :places, only: [:index, :show], param: :uri
+  end
 end
