2c7d7aada29734b58518f7b870cb582531b1b45f
Nikita Ignatovich <nikita@ignatovich.me>
2021-05-02 18:20:20 +0300

    Added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   namespace :v1 do
     resources :places, only: [:index, :show], param: :uri
+
+    controller :auth do
+      post 'autologin'
+      post 'signin'
+      delete 'signout'
+    end
+
+    controller :users do
+      post 'signup', to: 'users#create'
+      post 'user/confirm', to: 'users#confirm'
+      post 'user/reset_request', to: 'users#reset_request'
+      patch 'user/reset', to: 'users#reset'
+    end
   end
 end
