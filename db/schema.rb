2c893b82b034afb6890265e064d04c0f796566d8
Nikita Ignatovich <nikita@ignatovich.me>
2021-05-02 18:24:18 +0300

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_135844) do
+ActiveRecord::Schema.define(version: 2021_05_02_151448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -52,6 +52,33 @@ ActiveRecord::Schema.define(version: 2021_05_01_135844) do
     t.index ["uri"], name: "index_places_on_uri", unique: true
   end
 
+  create_table "tokens", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.integer "status", default: 1
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tokens_on_user_id"
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "fullname"
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.boolean "email_confirmed", default: false
+    t.uuid "email_confirmation_token", default: -> { "gen_random_uuid()" }
+    t.uuid "password_reset_token"
+    t.datetime "password_reset_sent_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email_confirmation_token"], name: "index_users_on_email_confirmation_token", unique: true
+    t.index ["password_reset_token"], name: "index_users_on_password_reset_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tokens", "users"
 end
