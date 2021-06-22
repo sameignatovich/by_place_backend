fe62a48f4e6190ee0f1c1bc1fcdc60017a528458
Nikita Ignatovich <nikita@ignatovich.me>
2021-06-22 14:05:13 +0300

    Autologins and  signin returns  avatar url

--- app/controllers/v1/auth_controller.rb
+++ app/controllers/v1/auth_controller.rb
@@ -6,7 +6,7 @@ class V1::AuthController < ApplicationController
     @user = User.find_by_email(user_params[:email])
     if @user and @user.authenticate(user_params[:password])
       token = jwt_token(@user)
-      render json: {token: token, user: {username: @user.username, fullname: @user.fullname}, message: 'Signin successful'}, status: :ok
+      render json: {token: token, user: {username: @user.username, fullname: @user.fullname, avatar: polymorphic_url(current_user.avatar)}, message: 'Signin successful'}, status: :ok
     else
       render json: {message: 'Wrong email or password'}, status: :unprocessable_entity
     end
@@ -21,7 +21,7 @@ class V1::AuthController < ApplicationController
 
   # POST /v1/autologin.json
   def autologin
-    render json: {user: {email: current_user.email, fullname: current_user.fullname, username: current_user.username}, loggedIn: true}, status: :ok
+    render json: {user: {email: current_user.email, fullname: current_user.fullname, username: current_user.username, avatar: polymorphic_url(current_user.avatar)}, loggedIn: true}, status: :ok
   end
 
   private
