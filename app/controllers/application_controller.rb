c496615704bc4a53723257962fba22673616ca7c
Nikita Ignatovich <nikita@ignatovich.me>
2021-08-11 19:35:21 +0300

    Used JWT method from separated file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,16 @@
 class ApplicationController < ActionController::API
   def authorization
-    render json: { message: 'Please sign in' }, status: :unauthorized unless logged_in?
-  end
-
-  def decoded_token
-    if auth_header
-      token = auth_header.split(' ')[1] # header: { 'Authorization': 'Bearer <token>' }
-      begin
-        token_hash = JWT.decode(token, jwt_secret, true, algorithm: 'HS256')
-        return token_hash[0]['data']
-      rescue JWT::DecodeError
-        nil
-      end
-    end
+    render json: { message: 'Unauthorized' }, status: :unauthorized unless logged_in?
   end
 
   def current_user
-    Token.find(decoded_token).user
+    Token.find(ApplicationJwt.decode(received_token)).user
   end
 
   private
-    def jwt_secret
-      return Rails.application.credentials.jwt_secret!
-    end
-
-    def auth_header
-      # { Authorization: 'Bearer <token>' }
-      request.headers['Authorization']
+    def received_token
+      #header: { 'Authorization': 'Bearer <token>' }
+      request.headers['Authorization'].split(' ')[1]
     end
 
     def logged_in?
